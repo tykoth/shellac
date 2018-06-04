0470c4f301a1a00346129336953240d2b40b7fa5
Thiago Bocchile <tykoth@gmail.com>
2018-06-04 09:02:01 -0300

    inject to github

--- extension/inject.js
+++ extension/inject.js
@@ -5,8 +5,36 @@
  */
 
 
-// if page is repository, button on top
-// if()
+
+function preguicajax( uri, data, opts )
+{
+  if (uri == null)
+    uri = '/';
+  if (opts == null)
+    opts = {};
+
+  var defaults = {
+    type: 'GET',
+    url: base_url+uri,
+    data: data,
+    dataType: 'html',
+    success: function(data) {
+      logger("ajax success");
+    },
+    error: function(xhr,textStatus) {
+      logger("ajax error:",xhr);
+    }
+  };
+
+  $.ajax($.extend({},defaults,opts));
+}
+function post_action(data)
+{
+  var payload = { action: data.action.name };
+  $.each( data.info, function(k,v) { payload["info."+k] = v; } );
+  $.each( data.tab, function(k,v) { payload["tab."+k] = v; } );
+  preguicajax('/action/', payload, { type:'POST' });
+}
 var ugly=[
     '<li>',
     '<div class="select-menu js-menu-container js-select-menu js-transitionable">',
@@ -21,9 +49,9 @@ console.log([jQuery, jQuery('.pagehead-actions').prepend(ugly)]);
 document.getElementById('the_button_who_will_clone_this_repo').onclick = function(){
     chrome.runtime.sendMessage('nkckncmnhgnilbbgdcpmdjihgdmhihed', {openUrlInEditor: 'aloalo'},
       function(response) {
-        if (!response.success){
+//        if (!response.success){
             alert('Errão'); //handleError('aloalo');
-        }
+//        }
           
       });    
 }
